7e2fdf3e5b74f79312dc74bd3aeae167b1241794
Toai Duong <dctoai1@gmail.com>
2019-03-21 08:47:35 +1100

    complete

---
+++ Form.PhoneWordApp/Form.PhoneWordApp/MainPage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Form.PhoneWordApp
+{
+	public class MainPage : ContentPage
+	{
+        Entry phoneNumberText;
+        Button buttonTranslate;
+        Button buttonCall;
+        string translateNumber;
+
+		public MainPage ()
+		{
+            this.Padding = new Thickness(20, 20, 20, 20);
+            StackLayout panel = new StackLayout();
+            panel.Spacing = 15;
+
+            panel.Children.Add(new Label { Text = "Enter a phone word", FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)) });
+
+            panel.Children.Add(phoneNumberText = new Entry { Text = "1-855-XAMARIN"});
+
+            panel.Children.Add(buttonTranslate = new Button { Text = "Translate Button"});
+            panel.Children.Add(buttonCall = new Button { Text = "Call", IsEnabled = false });
+
+            buttonTranslate.Clicked += ButtonTranslate_Clicked;
+
+            this.Content = panel;
+        }
+
+        private void ButtonTranslate_Clicked(object sender, EventArgs e)
+        {
+            translateNumber = Core.PhoneTranslatetor.toNumber(phoneNumberText.Text);
+
+            if (string.IsNullOrWhiteSpace(translateNumber))
+            {
+                buttonCall.IsEnabled = false;
+                buttonCall.Text = "Call";
+            }
+            else
+            {
+                buttonCall.IsEnabled = true;
+                buttonCall.Text = "Call " + translateNumber;
+            }
+        }
+    }
+}
---
+++ Form.PhoneWordApp/Form.PhoneWordApp/PhoneTranslatetor.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+
+namespace Core
+{
+    public static class PhoneTranslatetor
+    {
+        public static string toNumber(string raw)
+        {
+            if (string.IsNullOrWhiteSpace(raw))
+            {
+                return "";
+            }
+
+            raw = raw.ToUpperInvariant();
+
+            StringBuilder newNumber = new StringBuilder();
+
+            foreach (char c in raw)
+            {
+                if (" -0123456789".Contains(c))
+                {
+                    newNumber.Append(c);
+                }
+                else
+                {
+                    var result = TranslateToNumber(c);
+                    if (result != null)
+                        newNumber.Append(result);
+                    // Bad character?
+                    else
+                        return null;
+                }
+            }
+            return newNumber.ToString();
+        }
+
+        static bool Contains(this string keyString, char c)
+        {
+            return keyString.IndexOf(c) >= 0;
+        }
+
+        static readonly string[] digits = {
+            "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"
+        };
+
+        static int? TranslateToNumber(char c)
+        {
+            for (int i = 0; i < digits.Length; i++)
+            {
+                if (digits[i].Contains(c))
+                    return 2 + i;
+            }
+            return null;
+        }
+    }
+}
